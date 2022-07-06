f131e542d1f36c4ffbb2c48cb7098ca4a512e67f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-07-06 18:16:53 -0400

    replaced hardcoded renderSquare() calls with double for loop

--- src/board.tsx
+++ src/board.tsx
@@ -27,26 +27,30 @@ export const Board = (props: PropsType) => {
     } else {
         msg = `Next player: ${ props.player }`
     }
+    const nCols = 3;
+    const rows = [0, 1, 2].map(iRow => {
+        const cols = [0, 1, 2].map(iCol => {
+            const iSquare = iCol + iRow * nCols;
+            return (
+                <Square
+                    key={ iCol }
+                    value={ props.squares[iSquare] }
+                    clickHandler={ () => props.clickHandler(iSquare) }
+                />                
+            )
+        })
+        return (
+            <div key={iRow} className="board-row">
+                { cols }
+            </div>    
+        )
+    })
     return (
         <div>
             <div className="status">
                 { msg }
             </div>
-            <div className="board-row">
-                { renderSquare(props, 0) }
-                { renderSquare(props, 1) }
-                { renderSquare(props, 2) }
-            </div>
-            <div className="board-row">
-                { renderSquare(props, 3) }
-                { renderSquare(props, 4) }
-                { renderSquare(props, 5) }
-            </div>
-            <div className="board-row">
-                { renderSquare(props, 6) }
-                { renderSquare(props, 7) }
-                { renderSquare(props, 8) }
-            </div>
+            { rows }
         </div>
     );
 }
